d9e403689746b0ac87765b13fccb1f42b5488815
dynamyc <balint.sebestyen2004@gmail.com>
2023-04-19 09:29:06 +0200

    Add Material You auto-coloring support (very important!!)

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:dynamic_color/dynamic_color.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_mindfuck_painter/screens/home_page.dart';
 import 'package:flutter_mindfuck_painter/screens/login_page.dart';
@@ -17,23 +18,32 @@ void main() async {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
+  static final _defaultLightColorScheme =
+      ColorScheme.fromSwatch(primarySwatch: Colors.purple);
+
+  static final _defaultDarkColorScheme = ColorScheme.fromSwatch(
+      primarySwatch: Colors.purple, brightness: Brightness.dark);
+
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Mindfuck',
-      theme: ThemeData(
-        useMaterial3: true,
-        brightness: Brightness.light,
-        colorSchemeSeed: Colors.deepPurple,
-      ),
-      darkTheme: ThemeData(
-          useMaterial3: true,
-          brightness: Brightness.dark,
-          //colorSchemeSeed: Colors.purple,
-          backgroundColor: Color.fromARGB(255, 0, 0, 0)),
-      themeMode: ThemeMode.dark,
-      debugShowCheckedModeBanner: false,
-      home: tokenExists! ? HomePage() : LoginPage(),
-    );
+    return DynamicColorBuilder(
+        builder: ((lightDynamic, darkDynamic) => MaterialApp(
+              title: 'Mindfuck',
+              theme: ThemeData(
+                useMaterial3: true,
+                colorScheme: lightDynamic ?? _defaultLightColorScheme,
+                brightness: Brightness.light,
+                //  colorSchemeSeed: Colors.deepPurple,
+              ),
+              darkTheme: ThemeData(
+                  colorScheme: darkDynamic ?? _defaultDarkColorScheme,
+                  useMaterial3: true,
+                  brightness: Brightness.dark,
+                  //colorSchemeSeed: Colors.purple,
+                  backgroundColor: Color.fromARGB(255, 0, 0, 0)),
+              themeMode: ThemeMode.dark,
+              debugShowCheckedModeBanner: false,
+              home: tokenExists! ? HomePage() : LoginPage(),
+            )));
   }
 }
